803ae6e78e576825428ff62ac1155a820e7b1b10
eaorrr <czxx@EAOrrr.localdomain>
2025-12-03 15:33:56 +0800

    finish lab2: key/value server

--- kvsrv1/lock/lock.go
+++ kvsrv1/lock/lock.go
@@ -1,7 +1,8 @@
 package lock
 
 import (
-	"6.5840/kvtest1"
+	"6.5840/kvsrv1/rpc"
+	kvtest "6.5840/kvtest1"
 )
 
 type Lock struct {
@@ -11,6 +12,8 @@ type Lock struct {
 	// MakeLock().
 	ck kvtest.IKVClerk
 	// You may add code here
+	version rpc.Tversion
+	lockId  string
 }
 
 // The tester calls MakeLock() and passes in a k/v clerk; your code can
@@ -21,13 +24,40 @@ type Lock struct {
 func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
 	lk := &Lock{ck: ck}
 	// You may add code here
+	lk.lockId = l
+	lk.version = 0
 	return lk
 }
 
 func (lk *Lock) Acquire() {
 	// Your code here
+	for {
+		myValue := kvtest.RandValue(8)
+		err := lk.ck.Put(lk.lockId, myValue, lk.version)
+		if err == rpc.OK {
+			lk.version++
+			return
+		} else if err == rpc.ErrMaybe {
+			value, version, _ := lk.ck.Get(lk.lockId)
+			if value == myValue && version == lk.version+1 {
+				lk.version++
+				return
+			}
+		}
+		// fail to obtain lock
+		// sleep?
+		for {
+			value, version, _ := lk.ck.Get(lk.lockId)
+			lk.version = version
+			if value == "" { // someone release lock
+				break
+			}
+		}
+	}
+
 }
 
 func (lk *Lock) Release() {
 	// Your code here
+	lk.ck.Put(lk.lockId, "", lk.version)
 }
--- kvsrv1/server.go
+++ kvsrv1/server.go
@@ -6,9 +6,10 @@ import (
 
 	"6.5840/kvsrv1/rpc"
 	"6.5840/labrpc"
-	"6.5840/tester1"
+	tester "6.5840/tester1"
 )
 
+// const Debug = true
 const Debug = false
 
 func DPrintf(format string, a ...interface{}) (n int, err error) {
@@ -18,16 +19,21 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
-
+type DataValue struct {
+	version rpc.Tversion
+	value   string
+}
 type KVServer struct {
 	mu sync.Mutex
 
 	// Your definitions here.
+	data map[string]DataValue
 }
 
 func MakeKVServer() *KVServer {
 	kv := &KVServer{}
 	// Your code here.
+	kv.data = make(map[string]DataValue)
 	return kv
 }
 
@@ -35,6 +41,16 @@ func MakeKVServer() *KVServer {
 // exists. Otherwise, Get returns ErrNoKey.
 func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	dataValue, ok := kv.data[args.Key]
+	if !ok {
+		reply.Err = rpc.ErrNoKey
+	} else {
+		reply.Err = rpc.OK
+		reply.Value = dataValue.value
+		reply.Version = dataValue.version
+	}
 }
 
 // Update the value for a key if args.Version matches the version of
@@ -43,12 +59,43 @@ func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 // args.Version is 0, and returns ErrNoKey otherwise.
 func (kv *KVServer) Put(args *rpc.PutArgs, reply *rpc.PutReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	dataValue, ok := kv.data[args.Key]
+	if !ok {
+		if args.Version == 0 {
+			DPrintf("SUCCESS: receive PUT request with key = %v, version = %v for key nonexist", args.Key, args.Version)
+
+			reply.Err = rpc.OK
+			kv.data[args.Key] = DataValue{
+				version: 1,
+				value:   args.Value,
+			}
+		} else {
+			DPrintf("FAILED: receive PUT request with key = %v, version = %v for key nonexist", args.Key, args.Version)
+
+			reply.Err = rpc.ErrNoKey
+		}
+	} else {
+		if dataValue.version != args.Version {
+			DPrintf("FAILED: receive PUT request with key = %v, version = %v for key now with version = %v", args.Key, args.Version, dataValue.version)
+
+			reply.Err = rpc.ErrVersion
+		} else {
+			DPrintf("SUCCEE: receive PUT request with key = %v, version = %v for key now with version = %v", args.Key, args.Version, dataValue.version)
+
+			reply.Err = rpc.OK
+			kv.data[args.Key] = DataValue{
+				version: args.Version + 1,
+				value:   args.Value,
+			}
+		}
+	}
 }
 
 // You can ignore Kill() for this lab
 func (kv *KVServer) Kill() {
 }
-
 
 // You can ignore all arguments; they are for replicated KVservers
 func StartKVServer(ends []*labrpc.ClientEnd, gid tester.Tgid, srv int, persister *tester.Persister) []tester.IService {
